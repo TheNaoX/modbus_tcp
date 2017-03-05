a451fcd02753a11b5cbcac48bf79ab9f0d8ebbfd
Antonio Chávez <cavjzz@gmail.com>
2017-03-04 22:04:41 -0600

    Updated specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,16 @@
 require 'rack/test'
 require 'rspec'
 
+# Require the server file
 require File.expand_path '../../lib/server.rb', __FILE__
 
-module RSpecMixin
+# Include this rack/test module for using with RSpec
+module AppHelper
   include Rack::Test::Methods
   def app() Server end
 end
 
+# Include the helper module in rspec
 RSpec.configure do |c|
-  c.include RSpecMixin
+  c.include AppHelper
 end
